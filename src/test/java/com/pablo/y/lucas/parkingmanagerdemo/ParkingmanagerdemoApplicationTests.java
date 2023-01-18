3af932b0db71861ccb68743d21441159b0da5ffd
PabloFriasCampos <pfricam2505@g.educaand.es>
2023-01-18 19:32:41 +0100

    Commit inicial

---
+++ src/test/java/com/pablo/y/lucas/parkingmanagerdemo/ParkingmanagerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.y.lucas.parkingmanagerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingmanagerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
