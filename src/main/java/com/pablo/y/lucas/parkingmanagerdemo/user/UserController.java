5001ea7476029e00a80b32c8eb0c96f9002bf540
PabloFriasCampos <pfricam2505@g.educaand.es>
2023-01-25 20:51:47 +0100

    Landing page + Visualizacion lista de usuarios

--- src/main/java/com/pablo/y/lucas/parkingmanagerdemo/user/UserController.java
+++ src/main/java/com/pablo/y/lucas/parkingmanagerdemo/user/UserController.java
@@ -1,9 +1,18 @@
 package com.pablo.y.lucas.parkingmanagerdemo.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class UserController {
-  
+  UserRepository repositiorio = new UserRepository();
+
+  @GetMapping("/usuarios")
+  public String displayUsers(Model model){
+    model.addAttribute("usuarios",repositiorio.getAll());
+    return "listausuarios";
+
+  }
 
 }
--- src/main/java/com/pablo/y/lucas/parkingmanagerdemo/user/UserRepository.java
+++ src/main/java/com/pablo/y/lucas/parkingmanagerdemo/user/UserRepository.java
@@ -1,11 +1,25 @@
 package com.pablo.y.lucas.parkingmanagerdemo.user;
 
 import java.util.List;
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserRepository {
-  //public List<User> getAll(){
-  
 
-  //}
+  List<User> usuarios = new ArrayList<>();
+
+
+  public List<User> getAll(){
+    usuarios.clear();
+    usuarios.add(new User("pablule@gmail.com", "pablo"));
+    usuarios.add(new User("daniel@gmail.com", "dani"));
+    usuarios.add(new User("juanito@gmail.com", "juan"));
+    usuarios.add(new User("paquirri@gmail.com", "paco"));
+    usuarios.add(new User("deivid@gmail.com", "david"));
+    return usuarios;
+
+  }
 
 }
