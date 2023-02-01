c0cf391e9bec67de1bf2c0da4a93474c752a74c7
LucasBermudo <lberjun898@g.educaand.es>
2023-02-01 20:47:32 +0100

    Sorteo

---
+++ src/main/java/com/pablo/y/lucas/parkingmanagerdemo/sorteo/Sorteo.java
@@ -0,0 +1,34 @@
+package com.pablo.y.lucas.parkingmanagerdemo.sorteo;
+
+import java.util.Date;
+
+public class Sorteo{
+
+    private String fecha;
+    private String descripcion;
+    private String estado;
+
+    public Sorteo(String fecha, String descripcion, String estado;){
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+    public String getFecha(){
+        return fecha;
+    }
+    public void setFecha(String Fecha){
+        this.fecha = fecha;
+    }
+    public String getDescripcion(){
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion){
+        this.descripcion = descripcion;
+    }
+    public String getEstado(){
+        return estado;
+    }
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+}
