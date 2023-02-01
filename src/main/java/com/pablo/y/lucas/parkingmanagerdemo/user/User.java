a0999d3c47b4ecf9aa0401f5240915cb7a6d19dd
PabloFriasCampos <pfricam2505@g.educaand.es>
2023-02-01 20:48:51 +0100

    JUnit pom, User update, testsorteo y testuser

--- src/main/java/com/pablo/y/lucas/parkingmanagerdemo/user/User.java
+++ src/main/java/com/pablo/y/lucas/parkingmanagerdemo/user/User.java
@@ -4,31 +4,46 @@ public class User {
   //private long id;
   private String email;
   private String nombre;
+  private String apellido1;
+  private String apellido2;
 
-  public User(String email, String nombre){
+  public User(String email, String nombre, String apellido1, String apellido2) {
     this.email = email;
     this.nombre = nombre;
-
+    this.apellido1 = apellido1;
+    this.apellido2 = apellido2;
   }
 
-  public String getEmail(){
+  public String getEmail() {
     return email;
-    
   }
 
-  public String getNombre(){
-    return nombre;
-    
-  }
-
-  public void setEmail(String email){
+  public void setEmail(String email) {
     this.email = email;
-
   }
 
-  public void setNombre(String nombre){
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
     this.nombre = nombre;
-
   }
 
+  public String getApellido1() {
+    return apellido1;
+  }
+
+  public void setApellido1(String apellido1) {
+    this.apellido1 = apellido1;
+  }
+
+  public String getApellido2() {
+    return apellido2;
+  }
+
+  public void setApellido2(String apellido2) {
+    this.apellido2 = apellido2;
+  }
+  
 }
--- src/test/java/com/pablo/y/lucas/parkingmanagerdemo/TestUser.java
+++ src/test/java/com/pablo/y/lucas/parkingmanagerdemo/TestUser.java
@@ -1,14 +1,64 @@
 package com.pablo.y.lucas.parkingmanagerdemo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.pablo.y.lucas.parkingmanagerdemo.user.User;
 
 public class TestUser {
 
-  @Test
-  public void check_user_nombre(){
-    
+  private User user;
+
+  @BeforeEach
+  public void init(){
+    this.user = new User("email","nombre","apellido1","apellido2");
 
   }
 
+  @Test
+  public void check_user_nombre(){
+
+    String expected = "nombre";
+
+    String actual = user.getNombre();
+
+    assertEquals(expected, actual);
+
+  }
+
+  @Test
+  public void check_user_email(){
+
+    String expected = "email";
+
+    String actual = user.getEmail();
+
+    assertEquals(expected, actual);
+
+  }
+
+  @Test
+  public void check_user_apellido1(){
+
+    String expected = "apellido1";
+
+    String actual = user.getApellido1();
+
+    assertEquals(expected, actual);
+
+  }
+
+  @Test
+  public void check_user_apellido2(){
+
+    String expected = "apellido2";
+
+    String actual = user.getApellido2();
+
+    assertEquals(expected, actual);
+
+  }
 
 }
